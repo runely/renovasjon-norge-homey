668b04cb10645d4617c23247c1d4db3422dae9c7
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-12-23 10:49:07 +0100

    Moved all non-secret envs into config

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const Homey = require('homey')
 
 module.exports = {
   API: {
-    URL: Homey.env.API_URL,
-    FRACTIONS: Homey.env.API_FRACTIONS,
-    CALENDAR: Homey.env.API_CALENDAR
+    URL: Homey.env.API_URL || 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api',
+    FRACTIONS: Homey.env.API_FRACTIONS || 'fraksjoner',
+    CALENDAR: Homey.env.API_CALENDAR || 'tommekalender'
   },
   HEADER: {
-    KEY_NAME: Homey.env.HEADER_KEY_NAME,
+    KEY_NAME: Homey.env.HEADER_KEY_NAME || 'RenovasjonAppKey',
     KEY_VALUE: Homey.env.HEADER_KEY_VALUE,
-    COUNTY_NAME: Homey.env.HEADER_COUNTY_NAME
+    COUNTY_NAME: Homey.env.HEADER_COUNTY_NAME || 'Kommunenr'
   },
-  API_GEONORGE_URL: Homey.env.API_GEONORGE_URL
+  API_GEONORGE_URL: Homey.env.API_GEONORGE_URL || 'https://ws.geonorge.no/adresser/v1/sok?sok='
 }
